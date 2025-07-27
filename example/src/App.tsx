791e8c698aa5b08f62fb8733e6371fac533d0835
Gennady Vasilev <whidrubeld@gmail.com>
2025-07-28 01:10:13 +0300

    feat(types): add ModalProps interface and related types for modal functionality

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,7 @@
-import { Text, View, StyleSheet } from 'react-native';
-import { multiply } from 'react-native-reanimated-modal';
-
-const result = multiply(3, 7);
+import { View, StyleSheet } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
-  );
+  return <View style={styles.container}></View>;
 }
 
 const styles = StyleSheet.create({
---
+++ src/types.ts
@@ -0,0 +1,46 @@
+import type {
+  ModalPropsAndroid,
+  ModalPropsIOS,
+  StyleProp,
+  ViewStyle,
+} from 'react-native';
+
+export type ModalAnimation = 'fade' | 'slide';
+export type SwipeDirection = 'up' | 'down' | 'left' | 'right';
+
+export interface ModalProps
+  extends Pick<ModalPropsIOS, 'onOrientationChange' | 'supportedOrientations'>,
+    Pick<
+      ModalPropsAndroid,
+      | 'hardwareAccelerated'
+      | 'navigationBarTranslucent'
+      | 'statusBarTranslucent'
+    > {
+  isVisible: boolean;
+  children: React.ReactNode;
+  style?: StyleProp<ViewStyle>;
+  contentContainerStyle?: StyleProp<ViewStyle>;
+
+  // Animation type
+  animation?: ModalAnimation;
+  animationDuration?: number;
+
+  // Backdrop related
+  hasBackdrop?: boolean;
+  backdropColor?: string;
+  backdropOpacity?: number;
+  onBackdropPress?: () => void;
+
+  // Swipe
+  swipeDirection?: SwipeDirection | SwipeDirection[];
+  swipeThreshold?: number; // in pixels
+  swipeEnabled?: boolean;
+
+  // Others
+  coverScreen?: boolean; // If true, covers the entire screen
+
+  // Events
+  onShow?: () => void;
+  onHide?: () => void;
+  onRequestClose?: () => void;
+}
