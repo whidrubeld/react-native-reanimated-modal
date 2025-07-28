f1f4b266262b31a8ea9affac9d11a28c522f4e93
Gennady Vasilev <whidrubeld@gmail.com>
2025-07-28 12:42:03 +0300

    test: add setupTests.js for Jest configuration and mocking

---
+++ setupTests.js
@@ -0,0 +1,10 @@
+/* global jest */
+import 'react-native-gesture-handler/jestSetup';
+
+// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+jest.mock('react-native-reanimated', () =>
+  require('react-native-reanimated/mock')
+);
+
+// Optional: mock NativeAnimatedHelper to silence warnings
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
