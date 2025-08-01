89d7da075ab5b4c5bb2726e39b0d34ef5a0ebd29
Gennady Vasilev <whidrubeld@gmail.com>
2025-08-01 03:07:57 +0300

    chore: update typedoc theme

--- typedoc.mjs
+++ typedoc.mjs
@@ -1,13 +1,13 @@
 /** @type {Partial<import("typedoc").TypeDocOptions>} */
 const config = {
-  plugin: ['typedoc-github-theme'],
-  theme: 'typedoc-github-theme',
+  plugin: ['typedoc-theme-oxide'],
+  theme: 'oxide',
   includeVersion: true,
   navigation: {
     includeGroups: true,
   },
   //
-  name: 'react-native-reanimated-modal',
+  name: 'React Native Reanimated Modal',
   entryPoints: ['./src/index.ts'],
   out: 'docs',
   tsconfig: './tsconfig.json',
