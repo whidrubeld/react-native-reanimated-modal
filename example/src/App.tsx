38d3b1610ed756512935375c165a27d06e3be548
Gennady Vasilev <whidrubeld@gmail.com>
2025-10-16 21:53:57 +0300

    feat: add StatusBar component to App for improved status bar styling

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,11 +2,13 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { Content } from './Content';
+import { StatusBar } from 'expo-status-bar';
 
 const App = () => {
   return (
     <GestureHandlerRootView>
       <SafeAreaProvider>
+        <StatusBar style="dark" />
         <Content />
       </SafeAreaProvider>
     </GestureHandlerRootView>
