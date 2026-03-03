4513f9a8b541908e86c9847ba23280be85077f45
Gennady Vasilev <whidrubeld@gmail.com>
2026-03-03 12:57:04 +0300

    feat: enhance jest setup with mock for react-native-worklets

--- jest-setup.js
+++ jest-setup.js
@@ -1,2 +1,8 @@
+/* global jest */
+
 require('react-native-gesture-handler/jestSetup');
+
+jest.mock('react-native-worklets', () =>
+  require('react-native-worklets/src/mock')
+);
 require('react-native-reanimated').setUpTests();
