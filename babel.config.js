daf93b55fedaa8216817e3b23b155b1ea0ec076c
Gennady Vasilev <whidrubeld@gmail.com>
2026-03-03 14:24:16 +0300

    refactor: remove unused Flow plugin from devDependencies and clean up babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -10,27 +10,4 @@ module.exports = {
     },
   ],
   plugins: ['react-native-worklets/plugin'],
-  // TODO: Fix after upgrading to React Native 0.81.x (Flow syntax in core files)
-  env: {
-    test: {
-      presets: [
-        [
-          'module:@react-native/babel-preset',
-          {
-            unstable_disableES6Transforms: true,
-            flow: true,
-          },
-        ],
-      ],
-      plugins: [
-        'react-native-worklets/plugin',
-        [
-          '@babel/plugin-transform-flow-strip-types',
-          {
-            all: true,
-          },
-        ],
-      ],
-    },
-  },
 };
