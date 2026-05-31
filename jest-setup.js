89edd168b8d12131c01d155fc6c05b79e8a3389d
Gennady Vasilev <whidrubeld@gmail.com>
2026-05-31 23:07:09 +0300

    fix: update jest setup and dependencies for improved testing compatibility

--- jest-setup.js
+++ jest-setup.js
@@ -2,7 +2,11 @@
 
 require('react-native-gesture-handler/jestSetup');
 
-jest.mock('react-native-worklets', () =>
-  require('react-native-worklets/src/mock')
-);
+jest.mock('react-native-worklets', () => {
+  const mock = require('react-native-worklets/src/mock');
+  return {
+    ...mock,
+    scheduleOnRN: (fun, ...args) => fun(...args),
+  };
+});
 require('react-native-reanimated').setUpTests();
